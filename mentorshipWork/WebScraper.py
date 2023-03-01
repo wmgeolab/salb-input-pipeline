b762b6d3ce8d2f0b703878b3573fe360597d1ad3
kitoware <115099567+kitoware@users.noreply.github.com>
2023-03-01 14:45:52 -0500

    Update WebScraper.py

--- mentorshipWork/WebScraper.py
+++ mentorshipWork/WebScraper.py
@@ -3,6 +3,35 @@ from bs4 import BeautifulSoup as bs
 import re
 import csv
 import shutil
+import requests
+import urllib3
+import ssl
+
+
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
+
+
+def get_legacy_session():
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+    session = requests.session()
+    session.mount('https://', CustomHttpAdapter(ctx))
+    return session
+
+test = get_legacy_session().get("https://salb.un.org/en/data?title=&field_un_region_target_id=All&page=2")
+
+print(test)
+print(test.content)
 
 f = r"C:\Users\jkang\Documents\mentorshipWork\GeoJson.csv"
 rows = []
